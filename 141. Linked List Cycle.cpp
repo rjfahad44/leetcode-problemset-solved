217bc89dc1dbf407cb54b0d37bdc0b18371caded
Fahad_2022 <rjfahad44@gmail.com>
2023-03-28 23:19:39 +0600

    update

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,112 @@
+/* My Template (01-08-2022) */
+#include<iostream>
+#include<algorithm>
+#include <iomanip>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<limits>
+#include<iomanip>
+#include<numeric>
+#include<conio.h>
+using namespace std;
+
+#define in(x) cin>>x
+#define out(x) cout<<x
+#define to_str(x) # x
+#define concat(x,y) x ## y
+#define is_even_or_not(x) (x&1)
+#define showDecimalValues fixed << setprecision(12)
+#define D double
+#define lli long long int
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define mp map<string, ll>
+#define ump unordered_map<string,ll>
+#define ummp unordered_multimap<string, ll>
+#define vc vector<User>
+#define v2d vector<vector<int>>
+#define Permutation(ss) while(next_permutation(ss.begin(), ss.end()))
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forr(i, n) for (int i = n; i>=0; i--)
+#define optimize() \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0); \
+    cout.tie(0);
+#define testCase \
+    int t; \
+    cin>>t; \
+    while(t--)
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	  TreeNode():val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x):val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode * left, TreeNode * right):val(x), left(left), right(right) {}
+};
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x, ListNode* next) :
+	val(x), next(next) {}
+};
+
+
+class Solution {
+ public:
+  bool hasCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) return true;
+    }
+    return false;
+  }
+};
+
+void push(ListNode** head, int val)  
+{  
+    ListNode* new_node = new ListNode();
+    new_node->val = val;  
+    new_node->next = (*head);
+    (*head) = new_node;  
+}  
+
+void printList(ListNode *node)  
+{  
+    while (node)  
+    {  
+        cout<<" "<<node->val;
+        node = node->next; 
+    }  
+}  
+
+
+int main()
+{
+	int n;
+	Solution s;
+	ListNode *node = nullptr;
+	push(&node, 3);
+	push(&node, 2);
+	push(&node, 0);
+	push(&node, -4);
+	out(s.hasCycle(node)<<endl);
+	return 0;
+}
