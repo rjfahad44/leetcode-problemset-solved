71accfc8e3a4f5429866487d5ee40d8f10e7a2ea
Fahad_2022 <rjfahad44@gmail.com>
2023-04-06 22:39:03 +0600

    update

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,85 @@
+/* My Template (01-08-2022) */
+#include<iostream>
+#include<algorithm>
+#include <iomanip>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<limits>
+#include<numeric>
+#include<conio.h>
+#include <functional>
+
+using namespace std;
+
+#define in(x) cin>>x
+#define out(x) cout<<x
+#define to_str(x) # x
+#define concat(x,y) x ## y
+#define is_even_or_not(x) (x&1)
+#define showDecimalValues fixed << setprecision(12)
+#define D double
+#define lli long long int
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define mp map<string, ll>
+#define ump unordered_map<string,ll>
+#define umlp unordered_multimap<string, ll>
+#define iv vector<int>
+#define iv2d vector<vector<int>>
+#define Permutation(s) while(next_permutation(s.begin(), s.end()))
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forr(i, n) for (int i = n; i>=0; i--)
+#define optimize() \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0); \
+    cout.tie(0);
+#define testCase \
+    int t; \
+    cin>>t; \
+    while(t--)
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	  TreeNode():val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x):val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode * left, TreeNode * right):val(x), left(left), right(right) {}
+};
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x, ListNode* next) :
+	val(x), next(next) {}
+};
+
+
+
+
+int main()
+{
+	int n;
+	while(in(n)){
+		vector<int> v;
+		for(int i=0,a; i<n; i++, in(a), v.push_back(a));
+		int ans, c=0;
+		for(int i : v){
+			if(c==0) ans = i;
+			c += i==ans?1:-1;
+		}
+		out(ans<<endl);
+	}
+	return 0;
+}
