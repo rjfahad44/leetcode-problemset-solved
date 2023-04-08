b82d1de7985bd6867c817f8a2e011d246efc9a55
Fahad_2022 <rjfahad44@gmail.com>
2023-04-08 22:23:59 +0600

    update

---
+++ 190. Reverse Bits.cpp
@@ -0,0 +1,102 @@
+/* My Template (01-08-2022) */
+#include<iostream>
+#include<algorithm>
+#include <iomanip>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<limits>
+#include<numeric>
+#include<conio.h>
+#include <functional>
+
+using namespace std;
+
+#define in(x) cin>>x
+#define out(x) cout<<x
+#define to_str(x) # x
+#define concat(x,y) x ## y
+#define is_even_or_not(x) (x&1)
+#define showDecimalValues fixed << setprecision(12)
+#define D double
+#define lli long long int
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define mp map<string, ll>
+#define ump unordered_map<string,ll>
+#define umlp unordered_multimap<string, ll>
+#define iv vector<int>
+#define iv2d vector<vector<int>>
+#define Permutation(s) while(next_permutation(s.begin(), s.end()))
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forr(i, n) for (int i = n; i>=0; i--)
+#define optimize() \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0); \
+    cout.tie(0);
+#define testCase \
+    int t; \
+    cin>>t; \
+    while(t--)
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	  TreeNode():val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x):val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode * left, TreeNode * right):val(x), left(left), right(right) {}
+};
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x, ListNode* next) :
+	val(x), next(next) {}
+};
+
+
+
+
+int main()
+{
+	int n,k;
+	while(in(n>>k)){
+		//1st method
+		uint32_t res = 0;
+		for(int i=0; i<32; i++){
+			int bit = (n >> i) & 1;
+			res = res | (bit << (31 - i));
+		}
+		
+		//2nd method
+		/*uint32_t res = 0;
+        int x = 31;
+        while(x >= 0){
+            res = res ^ ((n & 0x1) << (x--));
+            n >>= 0x1;
+        }*/
+        
+        //3rd method
+        /*uint32_t res = 0;
+        for (int i = 0; i < 32; ++i){
+        	if (n >> i & 1){
+        		res |= 1 << 31 - i;
+        	}
+        }
+        */
+        
+		out(res<<endl);
+	}
+	return 0;
+}
